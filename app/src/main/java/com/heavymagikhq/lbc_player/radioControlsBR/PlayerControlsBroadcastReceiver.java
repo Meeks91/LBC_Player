89d15073723877e5f2172777f6ba62f8d944625b
Micah Simmons <micah.webber.simmons@gmail.com>
2017-09-09 18:12:32 +0100

    Updated icons, made app launch when the the notification is clicked on, refactored the project’s structure and updated the PlayerControlsBroadcastReceiver’s actions.

---
+++ app/src/main/java/com/heavymagikhq/lbc_player/radioControlsBR/PlayerControlsBroadcastReceiver.java
@@ -0,0 +1,60 @@
+package com.heavymagikhq.lbc_player.radioControlsBR;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import com.heavymagikhq.lbc_player.lbcStreamingService.RadioStreamingService;
+
+/**
+ * Created by Micah on 13/08/2017.
+ */
+
+public class PlayerControlsBroadcastReceiver extends BroadcastReceiver {
+
+    public static final String PAUSE_LBC_ACTION_KEY = "PAUSE_LBC_ACTION_KEY";
+    public static final String PLAY_LBC_ACTION_KEY = "PLAY_LBC_ACTION_KEY";
+    private RadioStreamingService radioStreamingService;
+
+    public PlayerControlsBroadcastReceiver(RadioStreamingService radioStreamingService) {
+
+        this.radioStreamingService = radioStreamingService;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //get the type of control being requested
+        final String actionType = intent.getAction();
+
+        //check if we should pause the lbc player
+        if (actionType.equals(PAUSE_LBC_ACTION_KEY))
+
+            radioStreamingService.getLbcExoPlayer().pauseLBC();
+
+
+        //check if we should play the lbc player
+        else if (actionType.equals(PLAY_LBC_ACTION_KEY))
+
+            radioStreamingService.getLbcExoPlayer().resumeLBC();
+    }
+
+    /**
+     * generates and returns an intentFilter with all of the acitons that
+     * the PlayerControlsBroadcastReceiver will listen to and handle
+     *
+     * @return - an intentFilter with the actions this broadcaster will response to
+     */
+    public IntentFilter generatePlayerControlsBroadcastReceiverIntentFilter(){
+
+        //create the intentFilter to intercept player controls
+        IntentFilter playerControlsBroadcastReceiverIntentFilter = new IntentFilter();
+
+        //create player control actions:
+        playerControlsBroadcastReceiverIntentFilter.addAction(PAUSE_LBC_ACTION_KEY);
+        playerControlsBroadcastReceiverIntentFilter.addAction(PLAY_LBC_ACTION_KEY);
+
+        return playerControlsBroadcastReceiverIntentFilter;
+    }
+}
---
+++ app/src/main/java/com/heavymagikhq/lbc_player/radioControlsBR/PlayerNotification.java
@@ -0,0 +1,86 @@
+package com.heavymagikhq.lbc_player.radioControlsBR;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+
+import com.heavymagikhq.lbc_player.LBCPlayerActivity;
+import com.heavymagikhq.lbc_player.R;
+import com.heavymagikhq.lbc_player.lbcStreamingService.RadioStreamingService;
+
+public class PlayerNotification{
+
+    /**
+     * creates and returns a Notification that's configured
+     * with actions to pause and play the LBC player
+     *
+     * @param context - context used to create the actions
+     * @return - Notification with the LBC controls
+     */
+    public static Notification build(Context context){
+
+        //create intent of where the PendingIntent should send the intent to
+        Intent notificationIntent = new Intent(context, LBCPlayerActivity.class);
+
+        //create action to pause the LBC player
+        NotificationCompat.Action pauseAction = createPauseAction(context);
+
+        //create action to play the LBC player
+        NotificationCompat.Action playAction = createPlayAction(context);
+
+        //Make pending intent to pass the notificationIntent
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //create the NotificationCompat to show the LBC controls and keep it in the forefront
+        return new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.lbc_icon)
+                .setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.colour_lbc_icon))
+                .setContentText("LBC Player")
+                .addAction(pauseAction)
+                .addAction(playAction)
+                .setPriority(Notification.PRIORITY_MAX)
+                .setWhen(0)
+                .setContentIntent(pendingIntent)
+                .build();
+    }
+
+    /**
+     * creates and returns an action to play the LBC player
+     *
+     * @param context - used to create the action
+     * @return - an action to play the LBC player
+     */
+    private static NotificationCompat.Action createPlayAction(Context context) {
+
+        //Create the action so it can be routed by the broadcast receiver
+        Intent playLBCIntent = new Intent(PlayerControlsBroadcastReceiver.PLAY_LBC_ACTION_KEY);
+
+        //create and return the NotificationCompat.Action
+        return new NotificationCompat.Action(R.drawable.play_icon,
+                                                "More LBC", PendingIntent.getBroadcast(context,
+                                                     0, playLBCIntent,
+                                                         PendingIntent.FLAG_UPDATE_CURRENT));
+    }
+
+    /**
+     * creates and returns an action to pause the LBC player
+     *
+     * @param context - used to create the action
+     * @return - an action to pause the LBC player
+     */
+    private static NotificationCompat.Action createPauseAction(Context context) {
+
+        //Create the action so it can be routed by the broadcast receiver
+        Intent pauseLBCIntent = new Intent(PlayerControlsBroadcastReceiver.PAUSE_LBC_ACTION_KEY);
+
+        //create and return the NotificationCompat.Action
+        return new NotificationCompat.Action(R.mipmap.ic_launcher,
+                                        "No More LBC", PendingIntent.getBroadcast(context,
+                                             0, pauseLBCIntent,
+                                                 PendingIntent.FLAG_UPDATE_CURRENT));
+    }
+}
